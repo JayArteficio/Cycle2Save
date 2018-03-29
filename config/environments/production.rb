47d37163ba948169ba3858dc9c54c2d696e257d4
Jay Arteficio <jarteficio@gmail.com>
2018-03-28 17:53:09 -0700

    change secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  config.strava_api_client_id = "21299"
-  config.strava_api_key = "67a0a31d505da6adf4ce3b0920786df219a7e113"
+  config.strava_api_client_id = ENV["strava_api_client_id"]
+  config.strava_api_key = ENV["strava_api_key"]
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
